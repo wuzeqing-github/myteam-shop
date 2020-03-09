b5cf02b9907ae2bf0a4646cfd16a75c5fee2d0a3
15571470192 <15571470192@163.com>
2020-03-09 18:51:45 +0800

    first construction

---
+++ my-project-zuul/src/main/java/com/qf/MyProjectZuulApplication.java
@@ -0,0 +1,13 @@
+package com.qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectZuulApplication.class, args);
+    }
+
+}
